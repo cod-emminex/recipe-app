94d2ce2b6c980b61694a2e4eb4fa0cfcab01173d
cod-emminex <emminexy@yahoo.com>
2025-01-04 20:25:00 +0100

    Added .env to gitignore file for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,10 +12,13 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect("your_mongo_db_connection_string", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://emminex:<admin@recipe-app.stvvj.mongodb.net/?retryWrites=true&w=majority&appName=Recipe-app",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
